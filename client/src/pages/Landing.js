8c285b1664639c9b7aac08b6ddec02188d174d58
Hello0801World <shun19036@yahoo.co.jp>
2023-06-06 14:01:24 +0900

    landing page and css

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,25 +1,32 @@
 import React from 'react'
 import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
+import { Logo } from '../components'
+
+import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
 
 const Landing = () => {
   return (
-    <main>
-      <nav>
-        <img src={logo} alt="jobify" />
-      </nav>
-      <div className="container page">
-      {/* Info */}
-        <div className="info">
-          <h1>Job<span>Tracking</span> app</h1>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
-          <button className="btn btn-here" >Login/Register</button>
+    <Wrapper>
+      <main>
+        <nav>
+          <Logo />
+        </nav>
+        <div className="container page">
+        {/* Info */}
+          <div className="info">
+            <h1>Job<span>Tracking</span> app</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+            <button className="btn btn-here" >Login/Register</button>
+          </div>
+          <img src={main} alt="main" className="img main-img" />
         </div>
-        <img src={main} alt="main" className="img main-img" />        
-      </div>
-    </main> 
+      </main> 
+
+    </Wrapper>
     
-  )
-}
+  );
+};
 
 export default Landing
